4004d96c567815c681a8bc0a38a3e6d92ff4d6b5
PromDex <86364561+PromDex@users.noreply.github.com>
2021-08-26 14:38:32 +0500

    PromDex-python-5-lesson

---
+++ example_5.py
@@ -0,0 +1,19 @@
+# 5. Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
+# Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+
+
+def summary():
+    try:
+        with open('example_5.txt', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print('Ошибка в файле')
+    except ValueError:
+        print('Не соблюдено условие ввода.')
+
+
+summary()
